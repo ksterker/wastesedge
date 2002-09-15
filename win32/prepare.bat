d760b22307ab503aaa84d0e08c4e2a0b77357fc3
Kai Sterker <kai.sterker@gmail.com>
2002-09-15 21:50:03 +0000

    ADDED win32 installer stuff

---
+++ win32/prepare.bat
@@ -0,0 +1,43 @@
+adonthell.exe -c
+
+cd modules
+..\adonthell.exe -c
+del adonthell.ini
+
+cd ..\games\wastesedge\scripts
+..\..\..\adonthell.exe -c
+del adonthell.ini
+
+cd actions
+..\..\..\..\adonthell.exe -c
+del adonthell.ini
+
+cd ..\dialogues
+..\..\..\..\adonthell.exe -c
+del adonthell.ini
+
+cd ..\game_events
+..\..\..\..\adonthell.exe -c
+del adonthell.ini
+
+cd ..\modules
+..\..\..\..\adonthell.exe -c
+del adonthell.ini
+
+cd ..\schedules
+..\..\..\..\adonthell.exe -c
+del adonthell.ini
+
+cd audio
+..\..\..\..\..\adonthell.exe -c
+del adonthell.ini
+
+cd ..\mapcharacters
+..\..\..\..\..\adonthell.exe -c
+del adonthell.ini
+
+cd ..\mapviews
+..\..\..\..\..\adonthell.exe -c
+del adonthell.ini
+
+exit
