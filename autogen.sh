b556a2cd586a5264d1306524a1a54d6ec78b1b46
Kai Sterker <kai.sterker@gmail.com>
2002-12-16 15:00:41 +0000

    FIXED autogen.sh (no longer deletes intl/Makefile.in)

--- autogen.sh
+++ autogen.sh
@@ -1,16 +1,35 @@
 #!/bin/sh
 # First of all clean up the generated crud
-rm -f configure config.log config.guess config.sub config.cache
-rm -f config.status
+rm -f configure config.log config.cache
+rm -f config.status aclocal.m4
+mv intl/Makefile.in intl/Makefile.bak
 rm -f `find . -name 'Makefile.in'`
+mv intl/Makefile.bak intl/Makefile.in
 rm -f `find . -name 'Makefile'`
 
 # Regenerate everything
-aclocal -I .
-automake --add-missing --gnu --copy
+
+#
+# use aclocal v1.6 if available
+#
+if test "x`which aclocal-1.6`" = "x" ; then
+  aclocal -I .
+else
+  aclocal-1.6 -I .
+fi
+
+#
+# use Autmake v1.6 if available
+#
+if test "x`which automake-1.6`" = "x" ; then
+  automake --add-missing --gnu --copy
+else
+  automake-1.6 --add-missing --gnu --copy
+fi
+
 autoconf 
 
 echo
-echo "Now type './configure' to prepare Waste's Edge for installation."
-echo "Afterwards, 'make install' will install it."
+echo "Now type './configure' to prepare Adonthell for compilation."
+echo "Afterwards, 'make' will build Adonthell."
 echo
